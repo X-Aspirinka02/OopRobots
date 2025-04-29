84a03e36fb59d4f936eee19dd563d2a52919f179
X-Aspirinka02 <az1610002@gmail.com>
2025-04-29 13:28:20 +0500

    javadoc на интерфейсы слушателей

--- src/main/java/log/LogChangeListener.java
+++ src/main/java/log/LogChangeListener.java
@@ -1,6 +1,11 @@
 package log;
-
+/**
+ * интерфейс для слушателя, подписавшегося на изменение логов
+ */
 public interface LogChangeListener
 {
+    /**
+     * метод, вызывающийся при изменении логов
+     */
     public void onLogChanged(); 
 }
--- src/main/java/state/LanguageChangeListener.java
+++ src/main/java/state/LanguageChangeListener.java
@@ -1,5 +1,11 @@
 package state;
 
+/**
+ * интерфейс для слушателя, подписавшегося на изменение языка
+ */
 public interface LanguageChangeListener {
+    /**
+     * метод, вызывающийся при изменении языка
+     */
     void onLanguageChanged();
 }
